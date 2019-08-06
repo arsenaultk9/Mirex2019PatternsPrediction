f2b147ff9f0b6e9460b5764ab3da731c8cc87339
KEARS4 <kevin.arsenault@dti.ulaval.ca>
2019-08-06 12:00:40 -0400

    Corrected note parser

--- src/note_parser.py
+++ src/note_parser.py
@@ -2,8 +2,8 @@ notes = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
 notes_and_octave = dict({})
 
 current_index = 0
-for note in notes:
-    for octave in range(11):
+for octave in range(11):
+    for note in notes:
         notes_and_octave[current_index] = note + str(octave)
         current_index += 1
 
