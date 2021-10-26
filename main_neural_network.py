7150183ffbfdfcbdb77ba4e7a705435e0fbe223f
arsenaultk9 <arsenaultk9@gmail.com>
2021-10-26 08:19:23 -0400

    Corrected usage of constants.

--- main_neural_network.py
+++ main_neural_network.py
@@ -20,7 +20,7 @@ file_name_a = 'data/PPDD-Sep2018_sym_mono_small/prime_csv/00b7561d-c09b-41f2-bf2
 file_name_b = 'data/PPDD-Sep2018_sym_mono_small/prime_csv/0b246118-2c95-4f4d-8e70-56e89f81fda2.csv'
 
 file_names = listdir(directory)  # [file_name_a, file_name_b]
-file_names = file_names[0:72]
+file_names = file_names[0:99]
 
 file_index = 0
 X = np.zeros((0, constants.WINDOW_SLIDE_SIZE,
@@ -66,7 +66,8 @@ print('===== Generation start =====')
 
 for continuation_index in range(42):
     continuation = network.generate_continuation(
-        cur_X[continuation_index], int(32 / constants.PREDICTION_SIZE))
+        cur_X[continuation_index], int(constants.WINDOW_SLIDE_SIZE / constants.PREDICTION_SIZE))
+
     continuation_unclustered = smnu.uncluster_song_notes(continuation)
 
     ig.sample_image('song_matrix_continuation_%d' %
